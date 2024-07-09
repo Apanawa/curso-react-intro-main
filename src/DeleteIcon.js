0225a58d56ae89fbeb9234f4a26e885fc600eeb5
Apanawa <ehecatl16@hotmail.com>
2024-07-09 15:30:05 -0600

    11 Iconos en React

---
+++ src/DeleteIcon.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {TodoIcon} from  "./TodoIcon"
+
+function DeleteIcon() {
+    return (
+        <TodoIcon
+            type="delete"  
+            color="red"
+        />
+    )
+}
+
+export { DeleteIcon }
