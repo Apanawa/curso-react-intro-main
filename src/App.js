ede0f86d6879721bf66af0b7a2f4f226620dc074
Apanawa <ehecatl16@hotmail.com>
2024-07-09 13:46:55 -0600

    9 Buscando ToDos

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
 
 const defaultTodos = [
-  {text: "Cortar Cebolla", completed: true},
+  {text: "Cortar cebolla", completed: true},
   {text: "Tomar el curso de Intro a React.js", completed: false},
   {text: "Llorar con la Llorona", completed: false},
   {text: "lalalalala", completed: false},
@@ -19,6 +19,14 @@ function App() {
 
   const completedTodos = todos.filter(todo => !!todo.completed).length
   const totalTodos = todos.length
+
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLowerCase()
+      const searchText = searchValue.toLowerCase()
+      return todoText.includes(searchText)
+    }
+  )
 
   console.log("los usuarios buscan todos de " + searchValue);
 
@@ -34,7 +42,7 @@ function App() {
       />
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem 
           key={todo.text}
           text={todo.text}
