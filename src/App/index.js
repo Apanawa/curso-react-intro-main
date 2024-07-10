7f4b2c3f86089962ee7b1a0672356a5eb59d5956
Apanawa <ehecatl16@hotmail.com>
2024-07-10 15:26:18 -0600

    17 abstraccion de componentes

--- src/App/index.js
+++ src/App/index.js
@@ -1,21 +1,6 @@
-import { TodoCounter } from '../TodoCounter';
-import { TodoSearch } from '../TodoSearch';
-import { TodoList } from '../TodoList';
-import { TodoItem } from '../TodoItem';
-import { CreateTodoButton } from '../CreateTodoButton';
 import React from 'react';
+import { AppUI } from './AppUI';
 import { useLocalStorage } from './useLocalStorage';
-
-// const defaultTodos = [
-// {text: "Cortar cebolla", completed: true},
-// {text: "Tomar el curso de Intro a React.js", completed: false},
-// {text: "Llorar con la Llorona", completed: false},
-// {text: "lalalalala", completed: false},
-// {text: "usar estados derivados", completed: true},
-// ]
-
-// localStorage.setItem("TODOS_V1", JSON.stringifydefaultTodos)
-// localStorage.removeItem("TODOS_V1")
 
 
 
@@ -53,33 +38,18 @@ function App() {
   }
 
   return (
-    <>
-      <TodoCounter
-       completed={completedTodos}
-       total={totalTodos}
-       />
-      <TodoSearch 
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem 
-          key={todo.text}
-          text={todo.text}
-          completed={todo.completed}
-          onComplete={ () => completeTodo(todo.text)}
-          onDelete={ () => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
-      <CreateTodoButton/>
-    </>
-  );
+    <AppUI
+      completedTodos ={completedTodos}
+      totalTodos ={totalTodos}
+      searchValue ={searchValue}
+      setSearchValue ={setSearchValue}
+      searchedTodos ={searchedTodos}
+      completeTodo ={completeTodo}
+      deleteTodo ={deleteTodo}
+    />
+  )
 }
-
+ 
 
 
 export default App;
