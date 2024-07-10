098ecde451c6979ec9809b247850816def6881c0
Apanawa <ehecatl16@hotmail.com>
2024-07-09 18:09:44 -0600

    12 Iconos con colores dinámicos

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,22 +5,16 @@ import './TodoItem.css';
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <CompleteIcon />
-      {/* <span 
-      className={`Icon Icon-check ${props.completed && "Icon-check--active"}`} onClick={props.onComplete}
-      >
-        V
-      </span> */}
+      <CompleteIcon 
+        completed={props.completed}
+        onComplete={props.onComplete}
+      />
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <DeleteIcon />
-     {/*  <span 
-        className="Icon Icon-delete"
-        onClick={props.onDelete}
-      >
-        X
-      </span> */}
+      <DeleteIcon 
+        onDelete={props.onDelete}
+      />
     </li>
   );
 }
